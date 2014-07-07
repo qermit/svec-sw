9d2a241c7301d98ffbfee1dc2e9db6c82029f9eb
Federico Vaga <federico.vaga@cern.ch>
2014-07-07 17:34:00 +0200

    kernel: incompatibility: partial revert 335e1019
    The patch 355e1019 move two fields in the spec_dev structure, but other
    drivers may include this header and use this structure, so it breakes
    all the fields's offset.
    
    Revert this modification in order to make the modification more backward
    compatible. It will not fix all the situations (sizeof) but some of them
    yes.
    
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- kernel/svec.h
+++ kernel/svec.h
@@ -66,11 +66,12 @@ struct svec_dev {
 	int lun;
 	int slot;
 	unsigned long flags;
+	char *fw_name;
 	struct device *dev;
 	char name[16];
 	char driver[16];
 	char description[80];
-
+	uint32_t fw_hash;
 	struct vme_mapping *map[__MAX_MAP];
 	struct svec_config cfg_cur, cfg_new;
 
@@ -87,8 +88,6 @@ struct svec_dev {
 	uint32_t vme_raw_addr;	/* VME address for raw VME I/O through vme_addr/vme_data attributes */
 	int verbose;
 
-	char *fw_name;
-	uint32_t fw_hash;
 	void *fw_buffer;
 	int fw_length;
 };
